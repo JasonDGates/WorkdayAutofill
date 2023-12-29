8758c4905767c85b2e558113c6ff043ef1ea43b7
Jason Gates <95332717+JasonDGates@users.noreply.github.com>
2023-12-28 21:03:36 -0600

    feat: adding first field selector

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if(request.message === "clicked_browser_action") {
         // Modify this section to fill fields on the website
-        document.getElementById('exampleField').value = 'Your Value';
+        document.querySelector('[name="01___title"]').value = 'Software Developer';
       }
     }
   );
