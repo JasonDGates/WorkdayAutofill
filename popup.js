12ab6828f476f563eb2c04c11f40d304497773c3
Jason Gates <95332717+JasonDGates@users.noreply.github.com>
2023-12-28 20:45:51 -0600

    feat: added popup.js

---
+++ popup.js
@@ -0,0 +1,7 @@
+// popup.js
+document.getElementById('fillButton').addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var activeTab = tabs[0];
+      chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+    });
+  });
