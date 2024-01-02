be5a0a425ac720d550b29cc404e31243176db9ca
Jason Gates <95332717+JasonDGates@users.noreply.github.com>
2024-01-02 08:35:37 -0600

    feat: adding dropdown selection

--- content.js
+++ content.js
@@ -5,8 +5,8 @@ chrome.runtime.onMessage.addListener(
         // Select and fill a field
         // document.querySelector('[name="01___title"]').value = 'Software Developer';
         document.querySelector('[id="input-3"]').value = "Jason";
-        const input1 = document.querySelector('[id="input-2"]');
-        input1.selectedIndex = 1;
+        document.querySelector('[id="input-2"]').click();
+        document.querySelector('[data-value="bc33aa3152ec42d4995f4791a106ed09"]').click();
         document.getElementById("input-4").value = "Gates";
         document.getElementById("input-5").value = "5402 FM 1488, 334";
         document.getElementById("input-6").value = "Magnolia";
