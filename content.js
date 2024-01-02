a72de071dfd863aa060a582b220b89990b3ad3d9
Jason Gates <95332717+JasonDGates@users.noreply.github.com>
2024-01-01 22:07:36 -0600

    feat: added more elements

--- content.js
+++ content.js
@@ -3,9 +3,15 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if(request.message === "clicked_browser_action") {
         // Select and fill a field
-        document.querySelector('[name="01___title"]').value = 'Software Developer';
+        // document.querySelector('[name="01___title"]').value = 'Software Developer';
+        document.getElementById("input-3").value = "Jason";
+        document.getElementById("input-4").value = "Gates";
+        document.getElementById("input-5").value = "5402 FM 1488, 334";
+        document.getElementById("input-6").value = "Magnolia";
+        document.getElementById("input-8").value = "77354";
+        document.getElementById("input-12").value = "(281) 799 5626";
         // Select and fill a radio button
-        document.querySelector('input[name="18orolder"]').checked = true;
+        // document.querySelector('input[name="18orolder"]').checked = true;
       }
     }
   );
